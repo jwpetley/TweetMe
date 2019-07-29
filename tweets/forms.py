504e22e2cf45f4c06c49ed0a6cf6c368b4273739
James Petley <James@student-10-245-174-19.wlan.dur.ac.uk>
2019-07-29 15:46:13 +0100

    Experimenting with different validation techniques.

--- tweets/forms.py
+++ tweets/forms.py
@@ -9,8 +9,8 @@ class TweetModelForm(forms.ModelForm):
         "user",
         "content",
         ]
-    def clean_content(self, *args, **kwargs):
-        content = self.cleaned_data.get("content")
-        if content == "abc":
-            raise forms.ValidationError("Cannot be ABC")
-        return content
+    #def clean_content(self, *args, **kwargs):
+    #    content = self.cleaned_data.get("content")
+    #    if content == "abc":
+    #        raise forms.ValidationError("Cannot be ABC")
+    #    return content
