9f1db1d6510336c54a7308b107c3f83700ac6de7
James Petley <James@student-10-245-174-19.wlan.dur.ac.uk>
2019-07-29 16:15:07 +0100

    Added some user authentication errors.

--- tweets/views.py
+++ tweets/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Tweet
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
 # Create your views here.
 
 #Create, Retrieve, Update, Delete
 
-class TweetCreateView(CreateView):
+class TweetCreateView(FormUserNeededMixin, LoginRequiredMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
-
+    login_url = '/admin/'
 
 
 class TweetDetailView(DetailView):
