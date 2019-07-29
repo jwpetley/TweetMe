fbd9db4750a7f4b8f66afc53d16c09bd2f9c837c
James Petley <James@student-10-245-174-19.wlan.dur.ac.uk>
2019-07-29 14:56:28 +0100

    Simple viewing using function based views.

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-
+from .models import Tweet
 # Create your views here.
 
 #Create, Retrieve, Update, Delete
 
 
 def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html", {})
+    obj = Tweet.objects.get(id=id)
+    context = {
+        "object" : obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
 
 def tweet_list_view(request,):
-    return render(request, "tweets/list_view.html", {})
+    queryset = Tweet.objects.all()
+    for obj in queryset:
+        print(obj.content)
+    context = {
+        "object_list" : queryset
+    }
+
+    return render(request, "tweets/list_view.html", context)
